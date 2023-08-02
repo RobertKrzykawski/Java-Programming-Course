7e545ac114f24b73aa029ebcce8503ab058aee9e
Robert <104578513+RobertKrzykawski@users.noreply.github.com>
2023-08-02 13:20:10 +0200

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,3 +1,13 @@
 public class Main {
-    System.out.print("Hello world");
+    public static void main(String[] args) {
+        //Account bobsAccount = new Account("12345", 1000, "Bob Brown", "myemail@bob.com", "(087) 123-4567");
+
+        Account bobsAccount = new Account();
+        System.out.println(bobsAccount.getNumber());
+        System.out.println(bobsAccount.getBalance());
+
+        bobsAccount.withdrawFunds(100.0);
+        bobsAccount.depositFunds(1000);
+        bobsAccount.withdrawFunds(500);
+    }
 }
