bfda7e0759c2a774c98481b7645f67dd5a81201e
Robert <104578513+RobertKrzykawski@users.noreply.github.com>
2023-08-03 13:16:25 +0200

    Add files via upload

---
+++ InheritanceChallange.java
@@ -0,0 +1,20 @@
+public class InheritanceChallange {
+    public static void main(String[] args) {
+        Employee tim = new Employee("Tim", "11/11/1985","01/01/2020");
+        System.out.println(tim);
+        System.out.println("Age = " + tim.getAge());
+        System.out.println("Pay = " + tim.collectPay());
+
+        SalariedEmployee joe = new SalariedEmployee("Joe", "11/11/1990", "03/03/2020", 35000);
+        System.out.println(joe);
+        System.out.println("Joe's paycheck = $" + joe.collectPay());
+        joe.retire();
+        System.out.println("Joe's Pension check = $" + joe.collectPay());
+
+        HourlyEmployee mary = new HourlyEmployee("Mary", "05/05/1970", "03/03/2021", 15);
+        System.out.println(mary);
+        System.out.println("Mary's paycheck = $" + mary.collectPay());
+        System.out.println("Mary's Holiday pay = $" + mary.getDoublePay());
+
+    }
+}
