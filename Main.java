b892677fb1bb66cf8c9574c00126efe3454cb1dc
Robert <104578513+RobertKrzykawski@users.noreply.github.com>
2023-08-14 13:16:02 +0200

    Add files via upload

--- Main.java
+++ Main.java
@@ -1,47 +1,27 @@
 package dev.lpa;
 
-import java.sql.SQLOutput;
 import java.util.Arrays;
+import java.util.Random;
 
 public class Main {
     public static void main(String[] args) {
-        int[] myInyArray = new int[10];
-        myInyArray[0] = 45;
-        myInyArray[1] = 1;
-        myInyArray[5] = 50;
+        System.out.println(Arrays.toString(getRandomArray(10)));
+    }
 
-        double[] myDoubleArray = new double[10];
-        myDoubleArray[2] = 3.5;
-        System.out.println(myDoubleArray[2]);
-
-        int[] firstTen = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
-        System.out.println("first = " + firstTen[0]);
-        int arrayLength = firstTen.length;
-        System.out.println("length of array = " + arrayLength);
-        System.out.println("last = " + firstTen[arrayLength - 1]);
-
-        int[] newArray;
-//        newArray = new int[]{5, 4, 3, 2, 1};
-        newArray = new int[5];
-        for (int i = 0; i < newArray.length; i++) {
-            newArray[i] = newArray.length - i;
+    public static int[] getRandomArray(int len) {
+        int[] newArray = new int[len];
+        Random random = new Random();
+        for (int i = 0; i < len; i++) {
+            newArray[i] = random.nextInt(1000);
         }
-        for (int i = 0; i < newArray.length; i++) {
-            System.out.print(newArray[i] + " ");
-        }
-        System.out.println();
-        for (int element : newArray) {
-            System.out.print(element + " ");
-        }
-        System.out.println();
         System.out.println(Arrays.toString(newArray));
-        Object objectVariable = newArray;
-        if (objectVariable instanceof int[]) {
-            System.out.println("objectVariable is really an int array");
+        Arrays.sort(newArray);
+        System.out.println(Arrays.toString(newArray));
+        for (int i = 0; i < len / 2; i++) {
+            int temp = newArray[i];
+            newArray[i] = newArray[len - 1 - i];
+            newArray[len - 1 - i] = temp;
         }
-
-        Object[] objectArray = new Object[3];
-        objectArray[0] = "Hello";
-        objectArray[1] = new StringBuilder("World");
+        return newArray;
     }
 }
