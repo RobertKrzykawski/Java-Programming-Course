1c9c041b82d45282c235615ccd19c88bc0a75fd0
Robert <104578513+RobertKrzykawski@users.noreply.github.com>
2023-08-17 15:44:10 +0200

    Add files via upload

--- MainEnum.java
+++ MainEnum.java
@@ -10,12 +10,29 @@ public class MainEnum {
 
         for (int i = 0; i < 10; i++) {
             weekDay = getRandomDay();
-            System.out.printf("Name is %s, Ordinal Value = %d%n",
-                    weekDay.name(), weekDay.ordinal());
 
-            if (weekDay == DayOfTheWeek.FRI) {
-                System.out.println("Found a Friday!!!");
-            }
+//            System.out.printf("Name is %s, Ordinal Value = %d%n",
+//                    weekDay.name(), weekDay.ordinal());
+//
+//            if (weekDay == DayOfTheWeek.FRI) {
+//                System.out.println("Found a Friday!!!");
+//            }
+            switchDayOfTheWeek(weekDay);
+        }
+
+        for (Topping topping : Topping.values()) {
+            System.out.println(topping.name() + " : " + topping.getPrice());
+        }
+    }
+
+    public static void switchDayOfTheWeek(DayOfTheWeek weekday) {
+        int weekDayInteger = weekday.ordinal() + 1;
+        switch (weekday) {
+            case WED -> System.out.println("Wednesday is Day " + weekDayInteger);
+            case SAT -> System.out.println("Saturday is Day " + weekDayInteger);
+            default -> System.out.println(weekday.name().charAt(0) +
+                    weekday.name().substring(1).toLowerCase() +
+                    "day is Day " + weekDayInteger);
         }
     }
 
