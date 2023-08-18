e2b57cde7549e970976a07607f0a07c35c5f2e9e
Robert <104578513+RobertKrzykawski@users.noreply.github.com>
2023-08-18 15:30:44 +0200

    Add files via upload

--- Animal.java
+++ Animal.java
@@ -35,29 +35,54 @@ record DragonFly(String name, String type) implements FlightEnabled {
 
 class Satellite implements OrbitEarth {
 
+    FlightStages stage = FlightStages.GROUNDED;
+
     public void achieveOrbit() {
-        System.out.println("Orbit achieved!");
+        transition("Orbit achieved!");
     }
 
     @Override
     public void takeOff() {
-
+        transition("Taking off");
     }
 
     @Override
     public void land() {
-
+        transition("Landing");
     }
 
     @Override
     public void fly() {
+        achieveOrbit();
+        transition("Data Collection while Orbiting");
+    }
 
+    public void transition(String description) {
+        System.out.println(description);
+        stage = transition(stage);
+        stage.track();
     }
 }
 
 interface OrbitEarth extends FlightEnabled {
 
     void achieveOrbit();
+    private static void log(String description){
+        var today = new java.util.Date();
+        System.out.println(today + " : " + description);
+    }
+
+    private void logStage(FlightStages stage, String description) {
+        description = stage + ": " + description;
+        log(description);
+    }
+
+    @Override
+    default FlightStages transition(FlightStages stage) {
+        FlightStages nextStage = FlightEnabled.super.transition(stage);
+        logStage(stage, "Beginning Transition to " + nextStage);
+        return nextStage;
+    }
 }
 
 interface FlightEnabled {
--- Test.java
+++ Test.java
@@ -3,6 +3,10 @@ package dev.lpa;
 public class Test {
     public static void main(String[] args) {
         inFlight(new Jet());
+
+//        OrbitEarth.log("Testing " + new Satellite());
+
+        orbit(new Satellite());
     }
 
     private static void inFlight(FlightEnabled flier) {
@@ -12,6 +16,12 @@ public class Test {
         if (flier instanceof Trackable tracked) {
             tracked.track();
         }
+        flier.land();
+    }
+
+    private static void orbit(OrbitEarth flier) {
+        flier.takeOff();
+        flier.fly();
         flier.land();
     }
 }
